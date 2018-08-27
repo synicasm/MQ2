7c9d7cc222d719f62076ac09386db216003585bc
synicasm <synicasm@gmail.com>
2018-08-27 09:52:02 -0700

    New MQ version

---
+++ Release/TeamXElcin.bat
@@ -0,0 +1,21 @@
+cls
+@ECHO off
+CD "C:\Users\Public\Daybreak Game Company\Installed Games\EverQuestMain\"
+timeout /t 5
+@ECHO Launching ShadowKnight - Skelcin
+START eqgame.exe patchme /login:synicasm1
+timeout /t 20
+@ECHO Launching Bard - Belcin
+START eqgame.exe patchme /login:synicasm22
+timeout /t 20
+@ECHO Launching Cleric - Celcin
+START eqgame.exe patchme /login:synicasm3
+timeout /t 20
+@ECHO Launching Wizard - Wzelcin
+START eqgame.exe patchme /login:synicasm4
+timeout /t 20
+@ECHO Launching Monk - Melcin
+START eqgame.exe patchme /login:synicasm5
+timeout /t 20
+@ECHO Launching Shaman - Shelcin
+START eqgame.exe patchme /login:synicasm6
